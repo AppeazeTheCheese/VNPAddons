7a93f535c79f9a0802988a853a2828da41ec9ec2
Cheese <yolosolodbaz@gmail.com>
2021-07-01 14:03:36 -0600

    Updated to work with 1.17

--- src/main/java/dev/appeazethecheese/vnpaddon/Main.java
+++ src/main/java/dev/appeazethecheese/vnpaddon/Main.java
@@ -9,6 +9,7 @@ import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.chat.ComponentSerializer;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.Console;
 import java.util.List;
 import java.util.Locale;
 import java.util.Objects;
@@ -27,7 +28,7 @@ public class Main extends JavaPlugin {
         getProtocolManager().addPacketListener(new PacketAdapter(StaticManager.Plugin, PacketType.Play.Server.CHAT) {
             @Override
             public void onPacketSending(PacketEvent event) {
-                if (event.getPacketType() != PacketType.Play.Server.CHAT) return;
+                if (event.getPacketType() != PacketType.Play.Server.CHAT && event.getPacketType() != PacketType.Play.Client.CHAT) return;
                 PacketContainer e = event.getPacket();
                 if (e.getChatComponents() == null) {
                     return;
@@ -36,6 +37,8 @@ public class Main extends JavaPlugin {
                 List<WrappedChatComponent> components = e.getChatComponents().getValues();
 
                 for (WrappedChatComponent wrappedChatComponent : components) {
+                    if(wrappedChatComponent == null)
+                        continue;
                     String json = wrappedChatComponent.getJson();
                     if (json == null || json.trim().isEmpty()) return;
                     TextComponent component = new TextComponent(ComponentSerializer.parse(json));
@@ -46,8 +49,9 @@ public class Main extends JavaPlugin {
                             plainText.toLowerCase(Locale.ROOT).contains("has made the advancement")) {
                         // Is an achievement announcement
                         String playerName = plainText.split(" ")[0];
-                        if (Objects.requireNonNull(StaticManager.getVnpManager()).isVanished(playerName))
+                        if (Objects.requireNonNull(StaticManager.getVnpManager()).isVanished(playerName)) {
                             event.setCancelled(true);
+                        }
                     }
                 }
             }
